c79751db9197d801e8dbaa8fac29dab0481a38cf
Oscar Lefebvre <oscar.lefebvre@skema.edu>
2015-11-27 19:56:02 +0100

    creation pro model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127185312) do
+ActiveRecord::Schema.define(version: 20151127185459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,24 @@ ActiveRecord::Schema.define(version: 20151127185312) do
     t.integer  "number_pers"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "pros", force: :cascade do |t|
+    t.string   "civility"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "company"
+    t.string   "job"
+    t.string   "email"
+    t.string   "phone"
+    t.string   "address"
+    t.string   "city"
+    t.integer  "zipcode"
+    t.string   "country"
+    t.string   "contact_by"
+    t.string   "request"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
