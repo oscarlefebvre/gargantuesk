6d51b1f7e6eb2b63930c3209c72b7b45e12dc807
Oscar Lefebvre <oscar.lefebvre@skema.edu>
2015-11-27 21:22:45 +0100

    creation of admin not finish

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ActiveRecord::Base
+  has_many :favourites
+  has_many :users, through: :favourites
+
+  validation :titre, presence: :true
+  validation :content, presence: :true
+  validation :category, presence: :true
 end
