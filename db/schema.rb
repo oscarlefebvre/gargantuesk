35d979c7f213c977df844076cc401a43d4d8b546
Oscar Lefebvre <oscar.lefebvre@skema.edu>
2015-11-27 19:47:23 +0100

    modif schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127175010) do
+ActiveRecord::Schema.define(version: 20151127182921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "titre"
+    t.string   "content"
+    t.string   "category"
+    t.date     "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
